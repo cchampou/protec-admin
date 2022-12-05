d93c561920a6d876de9dba8228493fbcfbb43e48
cchampou <clement@champouillon.com>
2022-12-05 21:49:51 +0100

    refactor: use block at a top level

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Event from './views/Event';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NewUser from './views/user/NewUser';
 import ListUsers from './views/user/ListUsers';
+import { Block } from 'baseui/block';
 
 const engine = new Styletron();
 
@@ -13,11 +14,13 @@ const App = () => {
     <BrowserRouter>
       <StyletronProvider value={engine}>
         <BaseProvider theme={LightTheme}>
-          <Routes>
-            <Route path="/user/new" element={<NewUser />} />
-            <Route path="/" element={<ListUsers />} />
-            <Route path="/event/new" element={<Event />} />
-          </Routes>
+          <Block width={['200px', '300px', '600px', '1000px']} margin="auto">
+            <Routes>
+              <Route path="/user/new" element={<NewUser />} />
+              <Route path="/" element={<ListUsers />} />
+              <Route path="/event/new" element={<Event />} />
+            </Routes>
+          </Block>
         </BaseProvider>
       </StyletronProvider>
     </BrowserRouter>
--- src/views/Event.tsx
+++ src/views/Event.tsx
@@ -1,21 +1,12 @@
 import { Button } from 'baseui/button';
 import Api from '../services/Api';
-import { DisplayXSmall, HeadingSmall } from 'baseui/typography';
+import { HeadingSmall } from 'baseui/typography';
 import { Input } from 'baseui/input';
 import { Card } from 'baseui/card';
 import { useStyletron } from 'baseui';
 import { Block } from 'baseui/block';
 import { Textarea } from 'baseui/textarea';
 import { DatePicker, TimePicker } from 'baseui/datepicker';
-
-const cardOverrides = {
-  Root: {
-    style: {
-      width: '500px',
-      margin: 'auto',
-    },
-  },
-};
 
 const Event = () => {
   const [css, theme] = useStyletron();
@@ -24,7 +15,7 @@ const Event = () => {
   };
 
   return (
-    <Card overrides={cardOverrides}>
+    <Card>
       <HeadingSmall>Nouveau déclenchement</HeadingSmall>
       <Block marginBottom={theme.sizing.scale400}>
         <Input placeholder="Titre" />
